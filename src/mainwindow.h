f81751dab94109f5b17c567ff6b63312f636f80a
eisaac23 <43345577+eisaac23@users.noreply.github.com>
2023-04-18 22:03:57 -0400

    added clear_graph slot

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -55,7 +55,7 @@ class MainWindow : public QMainWindow {
         void on_contactButton_clicked();
         void on_saveButton_clicked();
         void update_graph();
-
+        void clear_graph();
         void on_rechargeButton_clicked();
 
 private:
